084ef14488993736fce3bb4a5e8368d14ca41b60
fbrega10 <102411282+fbrega10@users.noreply.github.com>
2023-01-22 12:20:55 +0100

    added channels

--- Exercises/main.go
+++ Exercises/main.go
@@ -2,17 +2,16 @@ package main
 
 import "fmt"
 
-
-func main(){
+func main() {
 	mySlice := []int{}
-	for i := 0; i <= 10; i++{
+	for i := 0; i <= 10; i++ {
 		mySlice = append(mySlice, i)
 	}
 	// fmt.Println("Here's my slice of ints : \n",mySlice)
-	for i, v := range mySlice{
-		if v % 2 == 0{
+	for i, v := range mySlice {
+		if v%2 == 0 {
 			fmt.Println(i, v, " Even")
-		}else{
+		} else {
 			fmt.Println(i, v, " Odd")
 		}
 	}
---
+++ channels/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	links := []string{
+		"http://www.google.com",
+		"http://www.amazon.com",
+		"http://www.stackoverflow.com",
+		"http://www.golang.org",
+		"http://www.facebook.com",
+	}
+
+	c := make(chan string)
+
+	for i, link := range links {
+		go checkLink(link, c)
+		if i == 5{
+			close(c)
+		} 
+	}
+	index := 0
+	for element := range c {
+		go fmt.Println(element)
+		index++
+		if index == 5{
+			close(c)
+		}
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		c <- "server down"
+		return
+	}
+	c <- "server up: " + link
+}
