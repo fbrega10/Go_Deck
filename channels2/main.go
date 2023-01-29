85c15cc3de64e66c5c64fa375b0968ecd88bf131
fbrega10 <102411282+fbrega10@users.noreply.github.com>
2023-01-29 19:46:41 +0100

    select statement on servers slices

--- channels2/main.go
+++ channels2/main.go
@@ -7,28 +7,53 @@ import (
 )
 
 func main() {
-
 	start := time.Now()
-	links := []string{
+
+	tech := []string{
 		"http://www.google.com",
 		"http://www.amazon.com",
 		"http://www.stackoverflow.com",
 		"http://www.golang.org",
 		"http://www.facebook.com",
 	}
-	for _, i := range links{
-		checkLink(i)	
+
+	banking := []string{
+		"https://www.intesasanpaolo.com/",
+		"https://www.unicredit.it/",
+		"https://www.mediolanum.it/",
+		"https://www.generali.it/",
+		"https://www.allianz.it/",
+
+	}
+
+	c1, c2 := make(chan string), make(chan string)
+
+	for _, i := range tech{
+		go checkLink(i, c1)	
+	}
+
+	for _, s := range banking{
+		go checkLink(s, c2)
+	}
+
+	for i:=0; i < (len(tech)+ len(banking)); i++{
+		select {
+		case msg1 := <- c1:
+			fmt.Println("techs got first: ", msg1)
+		case msg2 := <- c2:
+			fmt.Println("banking got first: ", msg2)
+		}
 	}
 	elapsed := time.Since(start)
 	fmt.Println("program duration : ", elapsed)
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println("server down")
+		c <- "server down:" + link
 		return
 	}
-	fmt.Println("server up: " + link)
+	c <- "server up: " + link
 }
