7616c07b092bc64591d8658bbfa01a311f920d76
fbrega10 <102411282+fbrega10@users.noreply.github.com>
2023-02-02 09:05:41 +0100

    proto example

---
+++ testing/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	// "io"
+)
+
+func main(){
+
+	first := make([]string, 5)
+	first[0] = "salame"	
+	first[1] = "patate"	
+	first[2] = "carote"	
+	first[3] = "cipolle"	
+	first[4] = "pane"	
+}
